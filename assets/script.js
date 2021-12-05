d98701b5971d54ac3e2146d9b88e1dcdad4f1161
Emily Hammon <ehammon7@gmail.com>
2021-12-05 13:50:28 -0500

    fixed the styling for textarea depending on the time

--- assets/script.js
+++ assets/script.js
@@ -3,38 +3,46 @@ let today = moment().format('llll');
 
 $("#currentDay").text(today);
 
+let retrieveLocalStorage  = function() {
+    let key9 = JSON.parse(localStorage.getItem("storedItem9"));
+    $("#hour-9").val(key9);
+}
+
 //set color of rows based on current time
-function colorRow(time) {
-	var planNow = moment(now, "H A");
-	var planEntry = moment(time, "H A");
-	if (planNow.isBefore(planEntry) === true) {
-		return "future";
-	} else if (planNow.isAfter(planEntry) === true) {
-		return "past";
-	} else {
-		return "present";
-	}
-};
+let now = new Date();
+let hour = now.getHours();
+
+for (let i = 9; i < 18; i++) {
+    let currentId = "#hour-" + i;
+    let currentHourBlock = $(currentId);
+    // in the past
+    if (i < hour) {
+        currentHourBlock.addClass("past");
+    }
+    // in the future
+    else if (i > hour) {
+        currentHourBlock.addClass("future");
+    }
+    //current hour
+    else {
+        currentHourBlock.addClass("present");
+    }
+}
 
 //retrieve inputs from local storage
-function retrieveLocalStorage () {
-
-} 
-
-let key9 = JSON.parse(localStorage.getItem("storedItem9"));
-$("#hour-9").val(key9);
 
 
-$(".saveBtn").on("click", function(event) {
-    event.preventDefault(); 
-    alert("Hi");
-    let buttonID = $(this).attr("id");
+    let textarea9 = $("hour-9").val();
+    localStorage.setItem("storedItem9", JSON.stringify(textarea9))
+
+    console.log(textarea9);
 
 
-    let input9 = $("#hour-9").val();
-    localStorage.setItem("storedItem9", JSON.stringify(input9))
+    $(".saveBtn").on("click", function(event) {
+        event.preventDefault(); 
+        //alert("Hi");
+        let buttonID = $(this).attr("hour-9");
 
-    console.log(input9);
-})
-
+        
+    }); 
 retrieveLocalStorage();
